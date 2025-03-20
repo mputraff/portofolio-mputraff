ec5bdba3f7557a266c1df2dfdcf39c5f958589ed
putrafauzan32@gmail.com <email@example.com>
2025-03-20 15:40:54 +0700

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,66 @@
+const INFO = {
+	main: {
+		title: "Portofolio Putra Fauzan",
+		name: "Putra Fauzan",
+		email: "putrafauzan32@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/mputraff",
+		linkedin: "https://www.linkedin.com/in/mputraff",
+		instagram: "https://www.instagram.com/mputraff/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Web Developer",
+		description:
+			"I am an experienced Front End Developer with expertise in designing and developing interactive and responsive web applications using React.js. Strong understanding of user interface design principles and experience in implementing best practices in software development. Skilled in using HTML, CSS, and JavaScript to create intuitive and engaging user experiences. Experienced in working in multidisciplinary teams, collaborating with designers and backend developers to integrate APIs and ensure optimal application functionality. Always committed to improving skills and keeping up with the latest technologies in web development.",
+	},
+
+	about: {
+		title: "I’m Putra Fauzan. I live in Bandung, Indonesia",
+		description:
+			"I have a strong understanding of user interface design principles and experience applying best practices in software development. Skilled in using HTML, CSS, and JavaScript to create intuitive and engaging user experiences. Experienced in working in multidisciplinary teams, collaborating with designers and backend developers to integrate APIs and ensure optimal application functionality.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Chatter",
+			description:
+				"An interactive platform designed to allow users to share information, discuss, and interact in an online community. Similar to Kaskus, Chatter provides a space for users to create posts, provide comments, and like content shared by other members.",
+			logo: "https://static.cdnlogo.com/logos/r/85/react.svg",
+			linkText: "View Project",
+			link: "https://github.com/mputraff/chatter",
+		},
+
+		{
+			title: "Mama Care",
+			description:
+				"a mobile application specifically designed to support pregnant and breastfeeding mothers on their journey. The application provides a variety of features aimed at providing information, support, and community for mothers, so they can feel more confident and informed during pregnancy and breastfeeding.",
+			logo: "https://static.cdnlogo.com/logos/f/30/flutter.svg",
+			linkText: "View Project",
+			link: "https://github.com/mputraff/mama_care",
+		},
+
+		{
+			title: "Notepad",
+			description:
+				"a simple web application designed to provide users with an efficient note-taking and saving experience. With an intuitive interface, Notepad allows users to create, save, and manage their notes easily.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/mputraff/Notepad",
+		},
+	],
+};
+
+export default INFO;
